c5d8d2618d49ff14ccd7ad7e10d0789c5cdc4945
Anton-Mosquito <anton0886@outlook.com>
2022-01-10 15:15:32 +0200

    add models and actions

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const inject = require("require-all");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router;
 const port = 5000;
+const DB_URL = `mongodb+srv://user:123@cluster0.nm6ws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-//app.get();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
 try {
   const controllers = inject(__dirname + "/controllers");
+  const actions = inject(__dirname + "/actions");
+  const models = inject(__dirname + "/models");
+
+  mongoose.connect(DB_URL, { useNewUrlParser: true });
+
+  const db = mongoose.connection;
+
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.info("Database connected!");
+  });
 
   for (const key in controllers) {
     if (Object.hasOwnProperty.call(controllers, key)) {
-      app.use(`/${key}`, controllers[key](router));
+      app.use(`/${key}`, controllers[key]({ router, actions, models }));
     }
   }
 } catch (error) {
